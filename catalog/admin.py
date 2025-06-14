51bf0a10a70e9e1e891a95cecdb9274c6256e794
tmitev <todor.mitev@sofairo.com>
2025-06-14 14:41:48 +0300

    admin registered

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display =('id', 'category_name',)
+    search_fields = ('category_name', 'category_desc',)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'product_name', 'product_price', 'product_category',)
+    list_filter = ('product_category',)
+    search_fields = ('product_name', 'product_desc',)
+
