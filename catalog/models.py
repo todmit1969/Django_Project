d7fbbb6fcda1216f70bb397750bab218a9409f2b
tmitev <todor.mitev@sofairo.com>
2025-06-14 15:39:21 +0300

    screenshots added

--- catalog/models.py
+++ catalog/models.py
@@ -19,10 +19,10 @@ class Product(models.Model):
     product_name = models.CharField(max_length=50,verbose_name='Наименование')
     product_desc = models.CharField(max_length=150, verbose_name='Описание')
     product_image = models.ImageField()
-    product_category = models.ForeignKey(Category, on_delete=CASCADE, related_name='products', verbose_name='Категория')
+    product_category = models.CharField(max_length=50, verbose_name='Категория')
     product_price = models.FloatField(verbose_name='Цена')
     created_at = models.DateField(verbose_name='Дата создания')
-    update_at = models.DateField(verbose_name='Дата изменения')
+    updated_at = models.DateField(verbose_name='Дата изменения')
 
     def __str__(self):
         return f'{self.product_name}, {self.product_price}'
