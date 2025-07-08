0c69b939959ea4322de448ad6a6f51258ed68a2b
tmitev <todor.mitev@sofairo.com>
2025-07-08 13:04:43 +0300

    begining

--- catalog/models.py
+++ catalog/models.py
@@ -1,33 +1,33 @@
 from django.db import models
-from users.models import User
+from django.db.models import CASCADE
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=255, verbose_name="наименование категории")
-    description = models.TextField()
-
-    class Meta:
-        verbose_name = "Категория"
-        verbose_name_plural = "Категории"
+    name = models.CharField(max_length=100, verbose_name='Категория')
+    desc = models.CharField(max_length=200, verbose_name='Описание')
 
     def __str__(self):
-        return self.name
+        return  f'{self.name}, {self.desc}'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+        ordering = ['name']
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    created_at = models.DateTimeField(auto_now_add=True)
-    owner = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="products", null=True
-    )
-    is_published = models.BooleanField(default=False)
-
-    class Meta:
-        permissions = [
-            ("can_unpublish_product", "Can unpublish product"),
-        ]
+    name = models.CharField(max_length=50,verbose_name='Наименование')
+    desc = models.TextField(max_length=150, verbose_name='Описание')
+    image = models.ImageField(upload_to='catalog/images', blank=True, null=True)
+    price = models.FloatField(verbose_name='Цена')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата изменения')
+    category = models.ForeignKey(Category, on_delete=CASCADE, related_name='products')
 
     def __str__(self):
-        return self.name
+        return f'{self.name}, {self.price}'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+        ordering = ['name']
--- catalog/urls.py
+++ catalog/urls.py
@@ -1,35 +1,11 @@
-from django.urls import path
-from django.views.decorators.cache import cache_page
-
+from django.urls import path, include
 from catalog.apps import CatalogConfig
-from catalog.views import (
-    ProductListView,
-    ProductDetailView,
-    HomeView,
-    ContactsView,
-    ProductUpdateView,
-    ProductCreateView,
-    ProductDeleteView,
-    ProductUnpublishView,
-)
+from catalog.views import home, contacts, products_list
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path("", HomeView.as_view(), name="home"),
-    path("contacts/", ContactsView.as_view(), name="contacts"),
-    path("products/", ProductListView.as_view(), name="product_list"),
-    path("product/create/", ProductCreateView.as_view(), name="product_create"),
-    path("product/<int:pk>/", ProductDetailView.as_view(), name="one_product"),
-    path(
-        "product/<int:pk>/update/", ProductUpdateView.as_view(), name="product_update"
-    ),
-    path(
-        "product/<int:pk>/delete/", ProductDeleteView.as_view(), name="product_delete"
-    ),
-    path(
-        "product/<int:pk>/unpublish/",
-        ProductUnpublishView.as_view(),
-        name="product_unpublish",
-    ),
+    path('',home, name='home'),
+    path('contacts/',contacts, name='contacts'),
+    path('', products_list, name="products_list")
 ]
